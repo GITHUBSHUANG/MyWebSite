661a1f0a473d34b795902f79d316fedc7ecc2eda
82640 <82640@DESKTOP-NU7F7MH>
2017-12-29 09:13:50 +0800

    初始框架

---
+++ MyWebSite/Models/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace MyWebSite.Models
+{
+    //通过向ApplicationUser类添加属性，为应用程序用户添加概要数据
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
